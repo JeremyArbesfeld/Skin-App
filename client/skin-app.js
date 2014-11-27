7f7817e1a2b07c7ca7066e8d9548fa08207a1703
Family <family@Matthews-MacBook-Air-2.local>
2014-11-26 20:05:42 -0500

    New Format of Code

--- client/skin-app.js
+++ client/skin-app.js
@@ -1,203 +1,27 @@
-var questionType = new ReactiveVar("none");
-var resultPage = new ReactiveVar(false);
 var myPhoto = new ReactiveVar();
 
-var moleQuestions = [ 
-  { prompt: "Is it a new growth or mole?", 
-    state: new ReactiveVar("none"),
-    points: 14
-  }, 
-  { prompt: "Has the growth or mole changed recently?",
-    state: new ReactiveVar("none"),
-    points: 13 
-  },
-  {
-    prompt: "Have you ever been diagnosed with skin cancer?",
-    state: new ReactiveVar("none"), 
-    points: 12
-  },
-  {
-    prompt: "Is it bleeding?", 
-    state: new ReactiveVar("none"), 
-    points: 11 
-  },
-  { prompt: "Is it painful?", 
-    state: new ReactiveVar("none"), 
-    points: 10 
-  }, 
-  { 
-    prompt: "Is it chaning color?", 
-    state: new ReactiveVar("none"), 
-    points: 9 
-  }, 
-  { 
-    prompt: "Is it itchy?", 
-    state: new ReactiveVar("none"), 
-    points: 8 
-  }, 
-  { 
-    prompt: "Is it growing rapidly?", 
-    state: new ReactiveVar("none"), 
-    points: 7 
-  }, 
-  { 
-    prompt: "Does the growth or mole have an irregular border?", 
-    state: new ReactiveVar("none"), 
-    points: 6 
-  },
-  { 
-    prompt: "Is it larger than the size of a pencil eraser?", 
-    state: new ReactiveVar("none"), 
-    points: 5 
-  }, 
-  { 
-    prompt: "Do you have a history of blistering sunburns?", 
-    state: new ReactiveVar("none"), 
-    points: 4 
-  }, 
-  { 
-    prompt: "Do you have a history of indoor tanning?", 
-    state: new ReactiveVar("none"), 
-    points: 3 
-  }, 
-  { 
-    prompt: "Do you spend time sunbathing/getting suntanned?", 
-    state: new ReactiveVar("none"), 
-    points: 2 
-  }, 
-  { 
-    prompt: "Do you have a family history of skin cancer?", 
-    state: new ReactiveVar("none"), 
-    points: 1 
+Template.header.events({
+  'click #google': function () { 
+    if (Meteor.user()) { 
+      Router.go("/profilePage"); 
+    } else { 
+      Meteor.loginWithGoogle(); 
+    }
   }
+});
 
-]; 
-
-var rashQuestions = [ 
-  { prompt: "Is it a new rash?", 
-    state: new ReactiveVar("none"),
-    points: 14
-  }, 
-  {
-    prompt: "Is the rash/eruption painful?", 
-    state: new ReactiveVar("none"), 
-    points: 13 
-  }, 
-  { 
-    prompt: "Do you have a fever?", 
-    state: new ReactiveVar("none"), 
-    points: 12 
-  }, 
-  { 
-    prompt: "Is the rash/eruption red?", 
-    state: new ReactiveVar("none"), 
-    points: 11 
-  }, 
-  { 
-    prompt: "Is there any purlence (white, soft pus)?", 
-    state: new ReactiveVar("none"), 
-    points: 10 
-  }, 
-  {
-    prompt: "Are you experiencing itching?", 
-    state: new ReactiveVar("none"), 
-    points: 9 
-  }, 
-  { 
-    prompt: "Is there a red streak associated with the rash?", 
-    state: new ReactiveVar("none"), 
-    points: 8
-  }, 
-  { 
-    prompt: "Do you have an expanding target-like lesion?", 
-    state: new ReactiveVar("none"),
-    points: 7 
-  }, 
-  {
-    prompt: "Do you have a history of a recent insect bite or tick bite?", 
-    state: new ReactiveVar("none"), 
-    points: 6 
-  }, 
-  { 
-    prompt: "Are you taking any new medications?", 
-    state: new ReactiveVar("none"), 
-    points: 5 
-  }, 
-  { 
-    prompt: "Do you have a suppressed immune system?", 
-    state: new ReactiveVar("none"), 
-    points: 4 
-  },
-  { 
-    prompt: "Have you recently been gardening or hiking?", 
-    state: new ReactiveVar("none"), 
-    points: 3 
-  }, 
-  { 
-    prompt: "Do you have any other family members with similar symptoms?", 
-    state: new ReactiveVar("none"), 
-    points: 2 
-  }, 
-  { 
-    prompt: "Have you travelled recently?", 
-    state: new ReactiveVar("none"), 
-    points: 1 
-  }
-
-]; 
-
-var currentQuestions = function () {
-  if (questionType.get() === "mole") {
-    return moleQuestions;
-  } else if (questionType.get() === "rash") {
-    return rashQuestions;
-  } else {
-    return [];
-  }
-};
-
-Template.body.helpers ({ 
-  questions: currentQuestions,
-
-  hasQuestions: function () {
-    return questionType.get() !== "none";
-  },
-
-  isResultPage: function () {
-    return resultPage.get();
-  },
-
+Template.home.helpers ({ 
   currentPhoto: function () {
     return myPhoto.get();
   }
 });  
 
-var yesCount;
-
-Template.body.events( {
-  'click #submit': function () { 
-    console.log("cool");  
-
-    yesCount = 0;
-    _.each(currentQuestions(), function (question) {
-      if (question.state.get() === "yes") { 
-        yesCount = yesCount + question.points; 
-      }
-    }) 
-    resultPage.set(true);
-    console.log(yesCount)
-  },
-
+Template.home.events ( {
   'click #rash': function () { 
-    questionType.set("rash");
+    Router.go("questions", { type: "rash" });
   },
   'click #mole': function () {
-    questionType.set("mole");
-  },
-
-  'click #home': function () {
-    questionType.set("none");
-    resultPage.set(false);
+    Router.go("questions", { type: "mole" });
   },
 
   'click #picture': function () {
@@ -212,41 +36,49 @@ Template.body.events( {
         myPhoto.set(data);
     });
   }
+
 }); 
 
 Template.question.events ({ 
   'click #yes': function () {
-     console.log("hi"); 
-     this.state.set("yes")
+    Questions.update({_id: this._id}, 
+      { $set: {state: "yes"} } 
+    );
   },
 
   'click #no':  function () { 
-    console.log("bye");
-    this.state.set("no");  
+    Questions.update({_id: this._id}, 
+      { $set: {state: "no"} }
+    );
   },
 });
 
 Template.question.helpers ({ 
-  no: function() { 
-    return this.state.get() === "no"; 
+  no: function() {
+    return Questions.findOne({_id: this._id}).state === "no"; 
   },
   yes: function() { 
-    return this.state.get() === "yes"; 
+    return Questions.findOne({_id: this._id}).state === "yes"; 
   }
 });  
 
+Template.questionPage.events( {
+  'click #submit': function () {
+    Router.go("resultPage", { type: this.type });
+  }, 
+});
+
 Template.resultPage.helpers({
-  yesMessage: function () { 
-    console.log(yesCount);
-    if (90 <= yesCount) {
+  yesMessage: function () {
+    if (90 <= this.yesCount) {
       return "You should be seen immediately!"; 
-    } else if (65 <= yesCount) { 
+    } else if (65 <= this.yesCount) { 
       return "You should be seen within 1 day!"; 
-    } else if (40<= yesCount) { 
+    } else if (40<= this.yesCount) { 
       return "You should be seen within 2 days!"; 
-    } else if (20<= yesCount) { 
+    } else if (20<= this.yesCount) { 
       return "You should be seen within 5 days!"; 
-    } else if (10<= yesCount) { 
+    } else if (10<= this.yesCount) { 
       return "You should be seen within 1 week!"; 
     } else { 
       return "Call your dermatologist or primary care physician."; 
@@ -259,22 +91,23 @@ Template.resultPage.helpers({
     var firstLine = "";
     if (Meteor.user()) {
       userName = Meteor.user().profile.name;
-      firstLine = "Hi, this is " + userName + ".\n";
+      firstLine = "Hi, this is " + userName + "" + ".\n";
     }
-    if (90 <= yesCount) { 
+    if (90 <= this.yesCount) { 
       return firstLine + "I need to be seen immeadiately!"; 
-    } 
-    if (65 <== yesCount) {
+     
+    } else if (65 <=this.yesCount) {
       return firstLine + "I need to be seen within 1 day!"; 
-    } 
-    if (40 <= yesCount) { 
+     
+    } else if (40 <= this.yesCount) { 
       return firstLine + "I need to be seen within 2 days!"; 
-    } 
-    if (20 <= yesCount) { 
+     
+    } else if (20 <= this.yesCount) { 
       return firstLine + "I need to be seen within 5 days!"; 
-    } 
-    else { 
+     
+    } else { 
       return firstLine + "I have dermatologic symptoms and am unsure of their urgency."; 
+   }
   }
 })   
 
